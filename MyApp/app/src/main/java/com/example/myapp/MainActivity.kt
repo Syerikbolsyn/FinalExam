14b41a407d4a705d5695013bff14e082e793c8b4
Syerikbolsyn <meyrams@bk.ru>
2020-12-21 09:33:54 +0600

    labs

---
+++ MyApp/app/src/main/java/com/example/myapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.navigation.findNavController
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
